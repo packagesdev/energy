55e42b0f4a7202710ec5eef50b325c87da1f6c1b
packagesdev <stephane@MacPro.local>
2018-08-14 10:14:49 +0200

    Dark Appearance support implementation first step

--- Energy/NRGEnergySaverView.m
+++ Energy/NRGEnergySaverView.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2016, Stephane Sudre
+ Copyright (c) 2016-2018, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -263,6 +263,8 @@ static id _runningScreenSaverView=nil;
     
     if ([self isPreview]==YES)
     {
+		BOOL tIsDarkAppearance=[self NRG_isEffectiveAppareanceDarkAqua];
+		
 		BOOL tIsLimitedPowerSource;
 		
 		tIsLimitedPowerSource=(IOPSGetTimeRemainingEstimate()!=kIOPSTimeRemainingUnlimited);
